2a6c748b0ac2f36c1d2b27899f4607cd41f4bb60
sue-bin <sue_bin1029@yonsei.ac.kr>
2021-11-14 15:31:05 +0900

    feat: make service and get controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get() //if user come into this url //must go together with right bottom function (no black between)
+  getHello(): string { // excute this function
+    return this.appService.getHello(); // seperate business logic and controller by setting logics in service
   }
+
+  @Get("/hello")
+  sayHello():string {
+    return this.appService.getHi();
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController], //get url and run functions //similar to express router
   providers: [AppService],
 })
 export class AppModule {}
